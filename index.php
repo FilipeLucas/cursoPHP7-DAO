f639fb07fef7cd527810470a0b6a5076fdc43bdc
Filipe Ferreira <filipregunta@hotmail.com>
2020-02-15 22:42:04 -0300

    Seção 13 do curso - DAO

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
 require_once("config.php");
-
+/*
 $sql = new Sql();
 
 $usuarios = $sql-> select ("SELECT * FROM tb_usuarios");
 
-$usuarios = $sql->
-
 echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+
+$root->loadById(2);
+
+echo $root;
 
 ?>
