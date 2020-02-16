3be3a97b0ecb1037c0ceab000db72a7b04272bc9
Filipe Ferreira <filipregunta@hotmail.com>
2020-02-16 01:34:32 -0300

    Adicionados métodos de listagem

--- index.php
+++ index.php
@@ -7,12 +7,36 @@ $sql = new Sql();
 $usuarios = $sql-> select ("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+==========================================================
 */
 
+/*Retorna usuário especificado pelo ID
 $root = new Usuario();
 
 $root->loadById(2);
 
 echo $root;
+==========================================================
+*/
 
+/*Retorna uma lista de usuarios
+$lista = Usuario::getList();
+
+echo json_encode($lista);
+==========================================================
+*/
+
+/*Retorna uma lista de usuarios especificando pelo Login
+$search = Usuario::search("Mateus");
+
+echo json_encode($search);
+==========================================================
+*/
+
+/*Retorna usuário validando com login e senha por exemplo.
+$usuario = new Usuario();
+$usuario->login("josé", "123548");
+
+echo $usuario;
+*/
 ?>
